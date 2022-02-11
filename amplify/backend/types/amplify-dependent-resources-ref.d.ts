55b2afa659babd54336ecc2c2e5671f28c28cb3a
EC2 Default User <ec2-user@ip-172-31-10-94.ec2.internal>
2022-02-11 11:48:55 +0000

    amplify-graphql-starter

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "ConferenceAPI": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
